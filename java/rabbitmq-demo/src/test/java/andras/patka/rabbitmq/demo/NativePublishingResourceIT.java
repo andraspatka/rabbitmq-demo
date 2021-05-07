97a17c62cb8387afbab71ae76e35df4fe463cfc1
Patka Zsolt-Andras <andras.patka@gmail.com>
2021-05-07 16:32:48 +0300

    Dockerized quarkus app with jib

--- java/rabbitmq-demo/src/test/java/andras/patka/rabbitmq/demo/NativePublishingResourceIT.java
+++ java/rabbitmq-demo/src/test/java/andras/patka/rabbitmq/demo/NativePublishingResourceIT.java
@@ -3,7 +3,7 @@ package andras.patka.rabbitmq.demo;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativePublishingResourceIT extends PublishingResourceTest {
 
     // Execute the same tests but in native mode.
 }
---
+++ java/rabbitmq-demo/src/test/java/andras/patka/rabbitmq/demo/PublishingResourceTest.java
@@ -0,0 +1,9 @@
+package andras.patka.rabbitmq.demo;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class PublishingResourceTest {
+
+
+}
